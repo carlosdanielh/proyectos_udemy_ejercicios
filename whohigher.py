e5d2c19a1fe01582437e6a60dd83eacae86c0079
silverchair <carlosdanielh@mail.com>
2020-12-06 11:35:17 -0400

    whohigher funcionando,solo falta clean clode

--- whohigher.py
+++ whohigher.py
@@ -10,41 +10,43 @@ def random_number():
     return random.randint(0, 49)
 
 
-compare_a = data[random_number()]
-vs = data[random_number()]
+option_a = data[random_number()]
+name = option_a['name']
+description = option_a['description']
+country = option_a['country']
+followers_A = option_a['follower_count']
+
+option_b = data[random_number()]
+name_vs = option_b['name']
+description_vs = option_b['description']
+country_vs = option_b['country']
+followers_B = option_b['follower_count']
+
 count_score = 0
 chose_higher = False
 game_over = False
 while not game_over:
     os.system('cls')
     print(logo)
-    name = compare_a['name']
-    description = compare_a['description']
-    country = compare_a['country']
+    print(f'for debugger propose A has: {followers_A}'
+          f'followers  and B has {followers_B} followers')
     if chose_higher:
         print(f"You're right! Current score: {count_score}.")
     print(f'Compare A: {name}, a {description}, from {country}')
-
     print(vs_logo)
-    name_vs = vs['name']
-    description_vs = vs['description']
-    country_vs = vs['country']
     print(f'Against B: {name_vs}, a {description_vs}, from {country_vs}')
-
     answer = input("Who has more followers? Type 'A' or 'B': ").lower()
-
-    followers_A = compare_a['follower_count']
-    followers_B = vs['follower_count']
-    # check_if_is_higher(followers_A, followers_B)
 
     if answer == 'a':
         if followers_A > followers_B:
             count_score += 1
             chose_higher = True
-            vs = data[random_number()]
-            name_vs = vs['name']
-            description_vs = vs['description']
-            country_vs = vs['country']
+
+            option_b = data[random_number()]
+            name_vs = option_b['name']
+            description_vs = option_b['description']
+            country_vs = option_b['country']
+            followers_B = option_b['follower_count']
         else:
             os.system('cls')
             print(logo)
@@ -54,10 +56,17 @@ while not game_over:
         if followers_B > followers_A:
             count_score += 1
             chose_higher = True
-            compare_a = data[random_number()]
-            name = vs['name']
-            description = vs['description']
-            country = vs['country']
+
+            name = option_b['name']
+            description = option_b['description']
+            country = option_b['country']
+            followers_A = option_b['follower_count']
+
+            option_b = data[random_number()]            
+            name_vs = option_b['name']
+            description_vs = option_b['description']
+            country_vs = option_b['country']
+            followers_B = option_b['follower_count']
         else:
             os.system('cls')
             print(logo)
