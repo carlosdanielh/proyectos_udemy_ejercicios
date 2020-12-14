63ef001650561f90fc4e7d56f3c774851232acf9
silverchair <carlosdanielh@mail.com>
2020-12-13 21:35:13 -0400

    projecto 50% realizado

---
+++ paint_turtle.py
@@ -0,0 +1,15 @@
+import colorgram
+
+list_color = [(229, 228, 224), (46, 33, 27), (208, 170, 115), (235, 202, 218),
+            (177, 96, 48), (223, 231, 226)]
+# colors = colorgram.extract('descarga.jpg', 6)
+
+
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     lista_color = (r, g, b)
+#     list_color.append(lista_color)
+
+print(list_color)
