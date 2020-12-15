f83f57406f817f724fd181610e76f0031cc840bb
silverchair <carlosdanielh@mail.com>
2020-12-14 22:23:44 -0400

    paint project turtle module. finish 100%

--- paint_turtle.py
+++ paint_turtle.py
@@ -1,15 +1,39 @@
-import colorgram
+from turtle import Turtle
+import random
 
-list_color = [(229, 228, 224), (46, 33, 27), (208, 170, 115), (235, 202, 218),
-            (177, 96, 48), (223, 231, 226)]
-# colors = colorgram.extract('descarga.jpg', 6)
+# ##################################FUNCTIONS##################################
+def start_point(START_Y_POSITION):
+    tim.speed(0)    
+    tim.setpos(-300, START_Y_POSITION)
 
 
-# for color in colors:
-#     r = color.rgb.r
-#     g = color.rgb.g
-#     b = color.rgb.b
-#     lista_color = (r, g, b)
-#     list_color.append(lista_color)
+def leave_a_dot():
+    tim.screen.colormode(255)
+    tim.dot(20, random.choice(list_color))
 
-print(list_color)
+
+def move_foward():    
+    tim.forward(50)
+# ##############################END FUNCTIONS##################################
+
+
+list_color = [(46, 33, 27), (208, 170, 115), (235, 202, 218), (177, 96, 48),
+              (223, 231, 226)]
+
+START_Y_POSITION = -250
+UP_Y_POSITION = 50
+tim = Turtle()
+tim.up()
+
+start_point(START_Y_POSITION)
+leave_a_dot()
+
+for y in range(10):    
+    for x in range(9):
+        leave_a_dot()
+        move_foward()
+    START_Y_POSITION += UP_Y_POSITION
+    start_point(START_Y_POSITION)
+
+tim.hideturtle()
+tim.screen.exitonclick()
