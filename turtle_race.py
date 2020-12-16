b802f8b6f9660c523587dbbb9c0792d613d27aaf
silverchair <carlosdanielh@mail.com>
2020-12-15 21:02:43 -0400

    turtle terminado

--- turtle_race.py
+++ turtle_race.py
@@ -17,25 +17,28 @@ def race_turtle():
     got_to_goal = False
     while not got_to_goal:
         for turtle in list_turtles:
-            move_forward = random.randint(1, 15)
+            move_forward = random.choice(list_steps)
             turtle.forward(move_forward)
-
-            if turtle.xcor() >= 365:  # 375:
+            print(turtle.position())
+            if turtle.xcor() >= 365:
                 got_to_goal = True
                 color_turtle_who_won = turtle.pencolor()
+                break
     if bet_color == color_turtle_who_won:
-        print(f'you won! the {color_turtle_who_won} '
-              f'got to the goal')
+        turtle.write("YOU WON!!!", True, align="center",
+                     font=("Arial", 12, "normal"))
     else:
-        print(f'you lost! the {color_turtle_who_won} got to the goal')
+        turtle.write("YOU LOST!!!", True, align="center",
+                     font=("Arial", 8, "normal"))
 
 
 def main():
     global bet_color
     screen = Screen()
     screen.setup(800, 400)
-    bet_color = screen.textinput('Turtle Race', 'Who would you bet?'
-                                 'yellow, red, orange, purple, blue or pink'
+    screen.bgcolor('grey')
+    bet_color = screen.textinput('Turtle Race', 'Who would you bet? '
+                                 'yellow, red, orange, purple, blue or pink '
                                  'turtle?: ')
 
     start_point_y = -160
@@ -50,7 +53,7 @@ marly = Turtle('turtle')
 pluton = Turtle('turtle')
 wasock = Turtle('turtle')
 pepe = Turtle('turtle')
-print(pepe.shapesize())
+list_steps = [5, 10, 15, 20]
 list_turtles = [tim, jason, marly, pluton, wasock, pepe]
 ranbow_colors = ['yellow', 'red', 'orange', 'purple', 'blue', 'pink']
 main()
