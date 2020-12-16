bb4da68a8280fd2c2238f35e262d71912bec53d9
silverchair <carlosdanielh@mail.com>
2020-12-15 21:03:39 -0400

    en desarrollo el snake

---
+++ snake_turtle.py
@@ -0,0 +1,74 @@
+from turtle import Turtle, Screen
+
+
+def move(key_pressed):
+    global move_x
+    global move_y
+    game_over = False
+
+    while not game_over:
+        while True:
+
+            if key_pressed == 'right':
+                move_x += 20
+            elif key_pressed == 'up':
+                move_y += 20
+            elif key_pressed == 'left':
+                move_x -= 20
+            elif key_pressed == 'down':
+                move_y -= 20
+
+            if key_pressed == 'right':
+                for index, body in enumerate(body_snake):
+                    x = list_position[index][0] + move_x
+                    y = list_position[index][1] + move_y
+                    body.goto(x, y)
+            elif key_pressed == 'left':
+                for index in reversed(body_snake):
+                    x = list_position[index][0] + move_x
+                    y = list_position[index][1] + move_y
+                    body_snake[index].goto(x, y)
+
+
+def right():
+    move('right')
+
+
+def up():
+    move('up')
+
+
+def down():
+    move('down')
+
+
+def left():
+    move('left')        
+
+
+move_x = 0
+move_y = 0
+screen = Screen()
+screen.setup(width=500, height=500)
+screen.bgcolor('black')
+list_position = [(0, 0), (-20, 0), (-40, 0)]
+body_snake = []
+# screen.tracer(0)
+for position in list_position:
+
+    snake = Turtle()
+    
+    snake.up()
+    snake.shape('square')
+    snake.color('white')        
+    snake.goto(position)
+    body_snake.append(snake)
+
+# screen.update()
+screen.listen()
+screen.onkey(up, 'Up')
+screen.onkey(down, 'Down')
+screen.onkey(right, 'Right')
+screen.onkey(left, 'Left')
+
+screen.mainloop()
