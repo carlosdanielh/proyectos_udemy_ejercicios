12eb047d02d88fb393bb279b8a096c2bf0cd477c
silverchair <carlosdanielh@mail.com>
2020-12-17 17:02:22 -0400

    main.py snake avanzado,colision y random

--- snake_udemy/main.py
+++ snake_udemy/main.py
@@ -1,7 +1,7 @@
 from turtle import Screen
 from time import sleep
 from snake_class import Snake
-
+from Food import Food
 
 screen = Screen()
 screen.setup(width=500, height=500)
@@ -9,6 +9,7 @@ screen.tracer(0)
 screen.bgcolor('black')
 
 snake = Snake()
+food = Food()
 # snake.move('right')
 
 screen.listen()
@@ -16,11 +17,22 @@ screen.onkeypress(snake.up, 'Up')
 screen.onkeypress(snake.down, 'Down')
 screen.onkeypress(snake.right, 'Right')
 screen.onkeypress(snake.left, 'Left')
+screen.onkeypress(snake.pause, 'space')
+screen.onkeypress(snake.continue_, 'c')
+
 
 game_over = False
 while not game_over:
     screen.update()
     sleep(0.1)
-    snake.move()
+
+    if snake.activate:
+        snake.move()
+    print(snake.snake_body[0].distance(food))
+
+    if snake.collide(food):
+        food.random_move()
+
+
 
 screen.mainloop()
