55a7dfee34533181b78ad6e79c351f4699890982
silverchair <carlosdanielh@mail.com>
2020-12-19 21:05:54 -0400

    main.py ball.collide method

--- pong/main_pong.py
+++ pong/main_pong.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from table_pong import Table
 from time import sleep
+from ball import Ball
 WIDTH = 800
 HEIGHT = 600
 
@@ -18,14 +19,24 @@ table_left = Table()
 table_left.draw_paddle_left()
 
 screen.update()
-sleep(0.001)
+# sleep(0.01)
 screen.tracer(1)
 
 screen.listen()
 screen.onkeypress(table_right.up, 'Up')
 screen.onkeypress(table_right.down, 'Down')
-
 screen.onkeypress(table_left.up, 'w')
 screen.onkeypress(table_left.down, 's')
 screen.update()
+
+ball = Ball()
+game_over = False
+while not game_over:
+    sleep(0.01)
+    ball.move()
+
+    if ball.collide():
+        print('touch top')
+        game_over = True
+
 screen.mainloop()
