e268d588012088182a3349489c11a146bf6fdc82
silverchair <carlosdanielh@mail.com>
2020-12-22 11:56:53 -0400

    main.py 80% set run into turtle

--- crossing_turtle/main.py
+++ crossing_turtle/main.py
@@ -1,29 +1,34 @@
 from turtle import Screen
 from player import Player
 from score import Score
-
-
+from time import sleep
+from car_manager import Cars
 screen = Screen()
 screen.tracer(0)
 screen.setup(600, 600)
-
+cars = Cars()
 score = Score()
 screen.update()
+turtle = Player()
 
 screen.tracer(1)
-turtle = Player()
 screen.listen()
 screen.onkeypress(turtle.up, 'Up')
 screen.onkeypress(turtle.down, 'Down')
 
-if turtle.over_pass_top_screen:
-    print('entro')
-    score.set_point()
-    turtle.start_position()
+game_over = False
+while not game_over:
+    screen.update()
+    sleep(0.1)
+    cars.move()
 
-screen.update()
+    if turtle.over_pass_top_screen():
+        score.set_point()
+        turtle.start_position()
+    elif cars.run_into(turtle):
+        score.game_over_msg()
 
-# game_over = False
-# while not game_over:
+    # if cars.over_pass_left_screen():
+    #     cars.random_position()
 
 screen.mainloop()
