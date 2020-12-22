764479e9a3aff7687334fc6e04a15b23b4b2af79
silverchair <carlosdanielh@mail.com>
2020-12-22 11:57:52 -0400

    add start_position 80%

--- crossing_turtle/player.py
+++ crossing_turtle/player.py
@@ -9,18 +9,20 @@ class Player(Turtle):
         self.color('yellow')
         self.penup()
         self.goto(0, -280)
-        
+
     def up(self):
-        self.y = self.ycor() + 10
-        self.goto(0, self.y)
+        y = self.ycor() + 10
+        self.goto(0, y)
 
     def down(self):
-        self.y = self.ycor() - 5
-        self.goto(0, self.y)
+        y = self.ycor() - 10
+        self.goto(0, y)
 
     def over_pass_top_screen(self):
         if self.ycor() > 300:
             return True
 
     def start_position(self):
+        self.screen.tracer(0) 
         self.goto(0, -280)
+        self.screen.update()
