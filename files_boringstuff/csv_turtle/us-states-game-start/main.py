ef202df864b8317405d84942385292a566ef4cd0
silverchair <carlosdanielh@mail.com>
2020-12-31 15:42:30 -0400

    add saved list no guessed state to condition

--- files_boringstuff/csv_turtle/us-states-game-start/main.py
+++ files_boringstuff/csv_turtle/us-states-game-start/main.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from turtle import Screen, Turtle
 from state import State
 
+FONT_YOU_WON = ('Arial', 24, 'normal', 'bold')
+
 os.system('cls')
 windows = Screen()
 windows.tracer(0)
@@ -19,15 +21,21 @@ windows.update()
 state = State()
 want_to_exit = False
 while not want_to_exit:
-    answer_state = windows.textinput(title=state.guess_message(),
+    answer_state = windows.textinput(title=state.count_message(),
                                      prompt='Whats the other state\'s name '
                                      '(type nothing to exit)'
                                      )
 
     if len(answer_state) == 0:
         want_to_exit = True
+        state.saved_to_csv_all_state_no_guessed()
     elif state.check_state(answer_state):
         state.move_state_to_map(answer_state)
 
-windows.bye()
+    if len(state.guess_state_list) == 50:
+        turtle.goto(-100, 0)
+        turtle.color('red')
+        turtle.write('YOU WON!! THANK YOU FOR GAME', FONT_YOU_WON)
+
+
 windows.mainloop()
