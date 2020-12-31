5efb4e758a8aa094501058122e48b5e371c71979
silverchair <carlosdanielh@mail.com>
2020-12-30 20:44:59 -0400

    main.py finished, only clean code

---
+++ files_boringstuff/csv_turtle/us-states-game-start/main.py
@@ -0,0 +1,33 @@
+from turtle import Screen, Turtle
+from pathlib import Path
+from state import State
+import os
+
+os.system('cls')
+windows = Screen()
+windows.tracer(0)
+windows.setup(740, 540)
+turtle = Turtle()
+windows.title = ('U.S State')
+image = str(Path.cwd() / 'udemy_projects' / 'files_boringstuff' /
+            'csv_turtle' / 'us-states-game-start' /
+            'blank_states_img.gif'
+            )
+windows.addshape(image)
+turtle.shape(image)
+windows.update()
+state = State()
+want_to_exit = False
+while not want_to_exit:
+    answer_state = windows.textinput(title=state.guess_message(),
+                               prompt='Whats the other state\'s name'
+                               )
+
+    if len(answer_state) == 0:
+        want_to_exit = True
+    elif state.check_state(answer_state):
+        state.move_state_to_coordinate(answer_state)
+        windows.update()
+
+windows.bye()
+windows.mainloop()
