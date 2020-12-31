c13167bd662f8fddcfa7f1eb35d7c7afbd0cdf61
silverchair <carlosdanielh@mail.com>
2020-12-31 15:43:03 -0400

    add saved_no_guessed_state method

--- files_boringstuff/csv_turtle/us-states-game-start/state.py
+++ files_boringstuff/csv_turtle/us-states-game-start/state.py
@@ -36,4 +36,15 @@ class State(Turtle):
         x = int(self.data_states[self.data_states.state == answer].x)
         y = int(self.data_states[self.data_states.state == answer].y)
         self.goto(x, y)
-        self.write(answer, font=('Arial', 7, 'normal'))  
+        self.write(answer, font=('Arial', 7, 'normal'))
+
+    def saved_to_csv_all_state_no_guessed(self):
+        state_no_guessed_list = []
+        all_state_list = self.data_states.state.to_list()
+        the_path = str(Path.cwd() / 'data' / 'no_guessed_states.csv')
+
+        for state in all_state_list:
+            if state not in self.guess_state_list:
+                state_no_guessed_list.append(state)
+        no_guessed_state_csv = pandas.DataFrame(state_no_guessed_list)
+        no_guessed_state_csv.to_csv(the_path)
