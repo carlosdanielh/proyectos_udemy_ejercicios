a6a38d4fa01da1e83afda502ffccaf318303aa02
silverchair <carlosdanielh@mail.com>
2021-01-04 22:48:15 -0400

    clean code

--- files_boringstuff/csv_turtle/us-states-game-start/timer.py
+++ files_boringstuff/csv_turtle/us-states-game-start/timer.py
@@ -4,7 +4,7 @@ import time
 class CountDown:
     def __init__(self, time_secs):
         self.time_ = time_secs
-        
+
     def start_count_down(self):
         got_to_zero = False
         while not got_to_zero:
@@ -13,13 +13,12 @@ class CountDown:
             print(timer, end='\r')
             time.sleep(1)
             self.time_ -= 1
-            # import pdb; pdb.set_trace()
-            if mins == 0 and secs == 0:
+            if secs == 0:
                 got_to_zero = True
         return True
 
 
-times = CountDown(2)
+times = CountDown(1500)
 # tiempo = times.count_down()
 if times.start_count_down():
     print('perdiste')
