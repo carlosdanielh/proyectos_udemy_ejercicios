7a8126fa63602a280c97b66ef261350c808c9d11
silverchair <carlosdanielh@mail.com>
2021-01-05 23:04:44 -0400

    how create a img in a label tkinter working

--- timer_tomato_project/imagen.py
+++ timer_tomato_project/imagen.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from PIL import ImageTk, Image
 from pathlib import Path
-from img_class import PhotoMain
+
 PHOTO_PATH = str(Path.cwd() / 'img' / 'check.png')
 root= tk.Tk()
 
