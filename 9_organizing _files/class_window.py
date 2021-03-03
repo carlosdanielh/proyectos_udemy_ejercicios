4eca7201b438795aad8ebf246cb307d880f08943
silverchair <carlosdanielh@mail.com>
2021-03-02 23:01:44 -0300

    add a button to check if it works

--- 9_organizing _files/class_window.py
+++ 9_organizing _files/class_window.py
@@ -10,6 +10,12 @@ class Window(tk.Tk):
         ypos = (round((self.winfo_screenheight() / 2) - (self.height / 2)))-40
         self.geometry(f'{width}x{height}+{xpos}+{ypos}')
 
+        self.button = tk.Button(window,
+                           text='copy all',
+                           width=BUTTON_WIDTH,
+                           height=BUTTON_HEIGHT,
+                           command=copy_files)
+        button.grid(column=0, row=1, padx=10, pady=30)
 
 class ToplevelCustomize(tk.Toplevel):
     def __init__(self, width, height):
