e1d725ef44bc40d4820a63a4dd93d8e21fa72e1f
silverchair <carlosdanielh@mail.com>
2021-03-06 14:09:21 -0300

    buttons

--- 9_organizing _files/class_buttons.py
+++ 9_organizing _files/class_buttons.py
@@ -3,30 +3,24 @@ from class_miniwin import miniWin
 
 BUTTON_WIDTH = 15
 BUTTON_HEIGHT = 3
+button_clicked = False
 
 class buttonCustomize(tk.Button):
-    def __init__(self, master_, text_, task):
-        self.button = tk.Button(master_,
-                                text=text_,
-                                width=BUTTON_WIDTH,
-                                height=BUTTON_HEIGHT,
-                                command=lambda: self.click_button(task))
-        self.button.grid(column=0, row=1, padx=10, pady=30)
+    def __init__(self, master_, text_, command=None):
+        super().__init__(master_,
+                         text=text_,
+                         width=BUTTON_WIDTH,
+                         height=BUTTON_HEIGHT,
+                         
+                         )
 
     def click_button(self, option):
         if option == 'copy':
             b = miniWin('copy the selected')
+        elif option == 'move':
+            b = miniWin('move the selected')
+        button_clicked = True
 
-    # button_move = tk.Button(window,
-    #                         text='move files',
-    #                         width=BUTTON_WIDTH,
-    #                         height=BUTTON_HEIGHT,
-    #                         command=move_files)
-    # button_move.grid(column=1, row=1, padx=10, pady=10)
-
-    # button3 = tk.Button(window,
-    #                     text='xxxx',
-    #                     width=BUTTON_WIDTH,
-    #                     height=BUTTON_HEIGHT,
-    #                     command=copy_files)
-    # button3.grid(column=2, row=1, padx=10, pady=10)
+    def text(self):
+        print('entro')
+        return self['text']
